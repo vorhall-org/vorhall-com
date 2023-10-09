9b2c22b30e7501b5d2350c79bb24408323886455
Yves Tscherry <info@feerglas.com>
2023-10-09 17:17:43 +0200

    add i18n integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,25 @@
 import { defineConfig } from 'astro/config';
+import { i18n, filterSitemapByDefaultLocale } from 'astro-i18n-aut/integration';
+import sitemap from '@astrojs/sitemap';
+import { defaultLocale, locales, } from './src/i18n/utils';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://vorhall.com/",
+  trailingSlash: "never",
+  build: {
+    format: "file",
+  },
+  integrations: [
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),
+  ],
+});
