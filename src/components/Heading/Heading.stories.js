315eb1972f2657cbaaf330f9a367b543018c7153
Yves Tscherry <info@feerglas.com>
2024-01-15 16:57:38 +0100

    add arg-types

--- src/components/Heading/Heading.stories.js
+++ src/components/Heading/Heading.stories.js
@@ -1,6 +1,24 @@
 import Comp from './Heading.jsx';
 
 export default {
+  argTypes: {
+    level: {
+      control: {
+        max: 6,
+        min: 1,
+        step: 1,
+        type: 'number',
+      },
+    },
+    visualLevel: {
+      control: {
+        max: 6,
+        min: 1,
+        step: 1,
+        type: 'number',
+      },
+    },
+  },
   component: Comp,
 };
 
