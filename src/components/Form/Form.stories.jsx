dc4c5f56ccc85c3a1fc19fee14075b600be8c9e0
Yves Tscherry <info@feerglas.com>
2024-01-19 09:17:25 +0100

    remove contact form component and make it as story of form component

--- src/components/Form/Form.stories.jsx
+++ src/components/Form/Form.stories.jsx
@@ -1,4 +1,12 @@
 import Comp from './Form.jsx';
+import Fieldset from '../Fieldset/Fieldset.jsx';
+import * as fieldsetStories from '../Fieldset/Fieldset.stories.jsx';
+import InputText from '../InputText/InputText.jsx';
+import * as inputTextStories from '../InputText/InputText.stories.jsx';
+import Textarea from '../Textarea/Textarea.jsx';
+import * as textareaStories from '../Textarea/Textarea.stories.jsx';
+import Button from '../Button/Button.jsx';
+import * as buttonStories from '../Button/Button.stories.jsx';
 
 export default {
   component: Comp,
@@ -12,5 +20,23 @@ export const Form = {
   args: defaultArgs,
   render: (args) => <Comp {...args}>
     <p>some form content</p>
+  </Comp>,
+};
+
+export const ContactForm = {
+  args: {
+    ...defaultArgs,
+    button: buttonStories.Button.args,
+    classes: '',
+    fieldset: fieldsetStories.FieldsetWithHiddenLegend.args,
+    input: inputTextStories.EMailValidation.args,
+    textarea: textareaStories.Textarea.args,
+  },
+  render: (args) => <Comp>
+    <Fieldset {...args.fieldset}>
+      <InputText {...args.input} />
+      <Textarea {...args.textarea} />
+      <Button {...args.button} />
+    </Fieldset>
   </Comp>,
 };
