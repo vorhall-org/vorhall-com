80f3b11c53de59c2bcf8405bb869b325b465fb72
Yves Tscherry <info@feerglas.com>
2024-01-22 16:59:20 +0100

    use modular forms to create form based on config

--- src/components/Form/Form.stories.jsx
+++ src/components/Form/Form.stories.jsx
@@ -1,11 +1,7 @@
 import Comp from './Form.jsx';
-import Fieldset from '../Fieldset/Fieldset.jsx';
 import * as fieldsetStories from '../Fieldset/Fieldset.stories.jsx';
-import InputText from '../InputText/InputText.jsx';
 import * as inputTextStories from '../InputText/InputText.stories.jsx';
-import Textarea from '../Textarea/Textarea.jsx';
 import * as textareaStories from '../Textarea/Textarea.stories.jsx';
-import Button from '../Button/Button.jsx';
 import * as buttonStories from '../Button/Button.stories.jsx';
 
 export default {
@@ -18,25 +14,62 @@ const defaultArgs = {
 
 export const Form = {
   args: defaultArgs,
-  render: (args) => <Comp {...args}>
-    <p>some form content</p>
-  </Comp>,
 };
 
 export const ContactForm = {
   args: {
     ...defaultArgs,
-    button: buttonStories.Button.args,
-    classes: '',
-    fieldset: fieldsetStories.FieldsetWithHiddenLegend.args,
-    input: inputTextStories.EMailValidation.args,
-    textarea: textareaStories.Textarea.args,
+    button: {
+      ...buttonStories.Button.args,
+      click: false,
+      label: 'Submit',
+      type: 'submit',
+    },
+
+    fieldsets: [
+      {
+        fields: [
+          {
+            element: {
+              name: 'InputText',
+              props: {
+                ...inputTextStories.InputRequired.args,
+                name: 'email',
+                type: 'email',
+              },
+            },
+            name: 'email',
+            validate: [
+              {
+                text: 'Please enter your email.',
+                type: 'required',
+              },
+              {
+                text: 'The email address is badly formatted.',
+                type: 'email',
+              },
+            ],
+          },
+          {
+            element: {
+              name: 'Textarea',
+              props: {
+                ...textareaStories.Textarea.args,
+                required: true,
+              },
+            },
+            name: 'text',
+            validate: [
+              {
+                text: 'Please enter your request.',
+                type: 'required',
+              },
+            ],
+          },
+        ],
+        legend: fieldsetStories.FieldsetWithLegend.args,
+      },
+    ],
+
   },
-  render: (args) => <Comp>
-    <Fieldset {...args.fieldset}>
-      <InputText {...args.input} />
-      <Textarea {...args.textarea} />
-      <Button {...args.button} />
-    </Fieldset>
-  </Comp>,
 };
