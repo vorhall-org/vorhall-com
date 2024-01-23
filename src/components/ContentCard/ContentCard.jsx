f3e3dcc0cd2d3c4b5b2889cb91c6575ad684b249
Marius Bleuer <m.bleuer@lycos.com>
2024-01-23 17:36:00 +0100

    working on card variants

--- src/components/ContentCard/ContentCard.jsx
+++ src/components/ContentCard/ContentCard.jsx
@@ -1,5 +1,7 @@
 import styles from './ContentCard.module.scss';
+import Button from '../Button/Button';
 import Heading from '../Heading/Heading';
+import Icon from '../Icon/Icon';
 
 export default function ContentCard(props) {
   return (
@@ -9,12 +11,32 @@ export default function ContentCard(props) {
         [props.classes]: props.classes,
       }}
     >
-      <div class='card-card-wrapper'>
+      {props.icon &&
+        <div
+          classList={{
+            [styles['content-card--icon']]: true,
+          }}
+        >
+          <Icon {...props.icon} />
+        </div>
+      }
+      <div
+        classList={{
+          [styles['content-card--title']]: true,
+        }}
+      >
         <Heading {...props.heading} />
-        <p class='card-card-text'>
-          {props.text}
-        </p>
       </div>
+      <p
+        classList={{
+          [styles['content-card--lead']]: true,
+        }}
+      >
+        {props.text}
+      </p>
+      {props.button &&
+        <Button {...props.button} />
+      }
     </li>
   );
 }
