12bf63b610435a5f10e24dd209fcff9962937b70
Yves Tscherry <info@feerglas.com>
2024-01-23 15:18:51 +0100

    use link, add styling for lang switch

--- src/components/LangSwitch/LangSwitch.jsx
+++ src/components/LangSwitch/LangSwitch.jsx
@@ -1,18 +1,21 @@
 import styles from './LangSwitch.module.scss';
 import Icon from '../Icon/Icon';
+import Link from '../Link/Link';
 
 export default function LangSwitch(props) {
   return (
-    <a
-      classList={{
-        [styles['lang-switch']]: true,
-        [props.classes]: props.classes,
-      }}
-      href={props.href}
+    <Link
+      {...props.link}
+      classes={styles['lang-switch']}
     >
-      <Icon {...props.icon} />
-      {props.text}
-    </a>
+      <Icon
+        {...props.icon}
+        classes={styles['lang-switch__icon']}
+      />
+      <span>
+        {props.text}
+      </span>
+    </Link>
   );
 }
 
