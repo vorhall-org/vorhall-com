17fff080dfdf3588b03339e93a427e53e0e61065
Yves Tscherry <info@feerglas.com>
2024-01-31 15:00:30 +0100

    add missing slash

--- src/i18n/utils.js
+++ src/i18n/utils.js
@@ -8,6 +8,6 @@ export const locales = {
 export const useTranslations = (lang, translations) => (key) => translations[lang][key] || translations[defaultLocale][key];
 
 export const langRoute = (lang, route) => (lang === defaultLocale
-  ? `${route}/`
+  ? `/${route}/`
   : `/${lang}/${route}/`
 );
