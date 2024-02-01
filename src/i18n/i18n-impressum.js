69151c50a2cda679e7b8cf6d99757ea802e7cbef
Marius Bleuer <m.bleuer@lycos.com>
2024-02-01 16:19:16 +0100

    adding impressum rte content

--- src/i18n/i18n-impressum.js
+++ src/i18n/i18n-impressum.js
@@ -4,12 +4,13 @@ export default {
     docTitle: 'Impressum',
     lead: 'Impressum lead text',
     richtext: `
-<h2>Some random lorem ipsum</h2>
-<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Donec sed odio dui.</p>
-<p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id <a href="#">link</a> ultricies <b>vehicula</b> ut <i>id elit</i>. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-<h2>Some more random lorem ipsum</h2>
-<p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id <a href="#">link</a> ultricies <b>vehicula</b> ut <i>id elit</i>. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-    `,
+      <p><strong>Vorhall KLG</strong><br>
+      Brückfeldstrasse 40<br>CH-3012 Bern<br>Schweiz</p>
+      <p><strong>Kontaktmöglichkeit</strong><br>
+      info@vorhall.com</p>
+      <p><strong>UID</strong></strong><br>
+      CHE-371.533.033</p>
+`,
     title: 'Impressum',
   },
   en: {
@@ -17,11 +18,12 @@ export default {
     docTitle: 'Impressum',
     lead: 'Impressum lead text',
     richtext: `
-<h2>Some random lorem ipsum</h2>
-<p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec sed odio dui. Donec sed odio dui.</p>
-<p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id <a href="#">link</a> ultricies <b>vehicula</b> ut <i>id elit</i>. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-<h2>Some more random lorem ipsum</h2>
-<p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Etiam porta sem malesuada magna mollis euismod. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra augue. Maecenas sed diam eget risus varius blandit sit amet non magna. Nullam id dolor id <a href="#">link</a> ultricies <b>vehicula</b> ut <i>id elit</i>. Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+      <p><strong>Vorhall KLG</strong><br>
+      Brückfeldstrasse 40<br>CH-3012 Berne<br>Switzerland</p>
+      <p><strong>Contact</strong><br>
+      info@vorhall.com</p>
+      <p><strong>UID</strong></strong><br>
+      CHE-371.533.033</p>
     `,
     title: 'Impressum',
   },
