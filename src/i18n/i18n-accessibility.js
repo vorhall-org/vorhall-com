9f2c1d5ce7532e3a50fd5ffe44eb7661c062ac4d
Marius Bleuer <m.bleuer@lycos.com>
2024-02-01 22:18:08 +0100

    adding missing translation files

---
+++ src/i18n/i18n-accessibility.js
@@ -0,0 +1,84 @@
+export default {
+  de: {
+    description: 'Impressum beschreibung',
+    docTitle: 'Digitale Barrierefreiheit',
+    lead: `
+      Das Internet birgt ein immenses Potenzial für die Gleichstellung
+      der Menschen, insbesondere für Menschen mit Behinderungen. Indem
+      wir die technische Barrierefreiheit digitaler Produkte sicherstellen,
+      ermöglichen wir es möglichst vielen Menschen ein unabhängigeres
+      Leben zu führen und eigene Entscheidungen treffen zu können. Als
+      Produzenten digitaler Lösungen ist es unsere Verantwortung, unsere
+      Fähigkeiten bestmöglichst einzusetzen, um diese wesentliche
+      Barrierefreiheit bereitzustellen.
+    `,
+    richtext: `
+      <p>Einge gelungene Digitale Barrierefreiheit erfordert die
+      Zusammenarbeit aus unterschiedlichen Bereichen und
+      Perspektiven. Enstprechend ist es uns wichtig, die Barrierefreiheit
+      über die bloße Technik hinaus zu fördern und bei unseren Kundinnen
+      und Kunden möglichst nachhaltig in die Prozessen einfliessen zu
+      lassen. Wir verfügen über Erfahrung mit den populärsten
+      Desktop-Screenreadern wie JAWS und NVDA sowie mobile Varianten wie
+      Voice Over und Talkback. Im Rahmen unserer vergangenen Mandaten
+      haben wir eng mit Nutzerinnen und Nutzern von Screenreadern
+      zusammengearbeitet und dabei wertvolle Einblicke in ihre
+      Bedürfnisse und Präferenzen gewonnen. Dazu gehörten auch die
+      Durchführung von Live-Coding-Sitzungen, um die Effektivität unserer
+      Coding-Arbeit kontinuierlich zu überprüfen. Wir verfügen zudem über
+      Expertise in Bezug auf den schweizerischen Zertifizierungsprozess
+      gemäß den Web Content Accessibility Guidelines (WCAG).</p>
+      <h2>Wie wir Sie unterstützen können</h2>
+      <ul>
+        <li>Wir begleiten Ihren UX-Design Prozess um mögliche Accessibility
+        Probleme schon in der frühzeitig zu erkennen und zu vermeiden.</li>
+        <li>Wir leitsten Web-Entwicklungsarbeiten gemäss den neusten bekannten
+        Accessibility «Best Practices» und WCAG Guidelines.</li>
+        <li>Wir führen für Sie ein Stichprobenbasiertes Accessibility-Audit
+        Ihrer Website oder einzelner Features durch. Dabei testen wir manuell
+        in der zuvor festgelegten Screenreader-/Browserkombination. Sowohl
+        auf Desktop als auch auf iOS (Voice Over) oder Android (TalkBack).</li>
+        <li>Wir beraten Ihr Design System Team.</li>
+        <li>Wir helfen Ihnen die Digitale Barrierefreiheit nachhaltig zu
+        verankern.</li>
+      </ul>
+`,
+    title: 'Digitale Barrierefreiheit',
+  },
+  en: {
+    description: 'Impressum description',
+    docTitle: 'Digital Accessibility',
+    lead: `
+      The internet holds immense potential as an equalizer for many individuals,
+      particularly those with disabilities. By ensuring proper technical
+      accessibility of digital products, we can hopefully empower many individuals
+      to live independently and make their own choices. It is our duty to leverage
+      our abilities to provide this essential accessibility.
+    `,
+    richtext: `
+      <p>We welcome collaboration across various fields and perspectives to extend
+      accessibility beyond mere technicalities. Our proficiency spans popular desktop
+      screen readers such as JAWS and NVDA, as well as mobile ones including Voice Over and
+      Talkback. Throughout our professional journeys, we have engaged directly with screen
+      reader users, gaining valuable insights into their needs and preferences. This includes
+      conducting live coding sessions to verify the effectiveness of our efforts. Additionally,
+       we possess expertise in navigating the Swiss certification process based on the Web
+       Content Accessibility Guidelines (WCAG).</p>
+       <h2>What we can do for you</h2>
+       <ul>
+       <li>We accompany your UX design process to avoid the emergence of accessibility
+       problems in the first place.</li>
+       <li>We provide coding work according to accessibility best practices
+       and WCAG guidelines.</li>
+       <li>We conduct a sample-based accessibility audit of your website or specific
+       features on your behalf. During this process, we manually test using previously
+       agreed upon screen reader and browser combinations both on desktop and mobile
+       platforms such as iOS (VoiceOver) and Android (TalkBack).</li>
+       <li>We consult design system teams.</li>
+       <li>We help you to establish the subject of digital accessibility within the
+       organisation in a sustainable way.</li>
+       </ul>
+    `,
+    title: 'Digital Accessibility',
+  },
+};
---
+++ src/i18n/i18n-web-performance.js
@@ -0,0 +1,93 @@
+export default {
+  de: {
+    description: 'Impressum beschreibung',
+    docTitle: 'Web Performance (Monitoring)',
+    lead: `
+    Wir sehen das Thema Web-Performance als einen der entscheidenden
+    Erfolgsfaktoren für eine nachhaltige IT. Oftmals ist die
+    Web-Performance auch der Schlüsselfaktor für SEO, gute Core Web
+    Vitals-Werte und eine herausragende Kundenerfahrung für
+    die Nutzer:innen.
+    `,
+    richtext: `
+      <p>Wir helfen Ihnen dabei, Performance-Problem auf bestehenden
+      Plattformen zu analysieren und zu beheben. In neuen Projekten
+      können wir Sie bei der Erarbeitung von Architekturentscheidungen
+      begleiten sowie dann Aufbau eines umfassenden Frontend-Performance
+      Monitorings anbieten. Wir unterstützen Sie dabei, Ihre individuellen
+      Frontend Performance Metriken zu finden und zu definieren. Unsere
+      bisher umfangreichste Erfahrung im Frontend-Performance-Monitoring
+      konnten wir mit SpeedCurve sammeln. welches als eines der
+      renommiertesten Tools der Branche gilt. Sollten Sie mit einem
+      anderen Produkt arbeiten, sollten wir unser Wissen dennoch
+      adaptieren können. Wir bringen viel Erfahrung in der Arbeit mit
+      Content Delivery Networks (CDN) für die Optimierung und
+      Bereitstellung von Bildern mit. Diese Optimierung ist heutzutage
+      entscheidend um einerseits eine hervorragende UX-Experience als
+      auch einen minimalen ökologischen Fußabdruck zu gewährleisten.</p>
+      <p>Wir unterstützen Sie dabei, Ihre individuellen Frontend-Performance
+      Metriken zu finden und zu definieren. Unsere bisher umfangreichste
+      Erfahrung im Frontend-Performance Monitoring konnten wir mit
+      SpeedCurve sammeln. welches als eines der renommiertesten Tools
+      der Branche gilt. Sollten Sie mit einem anderen Produkt arbeiten,
+      sollten wir unser Wissen dennoch adaptieren können. Wir bringen
+      viel Erfahrung in der Arbeit mit Content Delivery Networks (CDN)
+      für die Optimierung und Bereitstellung von Bildern mit. Diese
+      Optimierung ist heutzutage entscheidend um einerseits eine
+      hervorragende UX-Experience als auch einen minimalen ökologischen
+      Fußabdruck zu gewährleisten.</p>
+      <h2>Wie wir Sie unterstützen können</h2>
+      <ul>
+        <li>Wir begleiten Ihre Architekturentscheide um mögliche
+        Performance Probleme schon im Ansatz zu vermeiden.</li>
+        <li>Wir identifizieren und analysieren Performance Probleme
+        auf existierenden Websites und Plattformen.</li>
+        <li>Wir leisten Web-Entwicklungsarbeiten gemäss den neusten
+        bekannten Web Performance «Best Practices»</li>
+        <li>Wir führen Web (Frontend) Performance Audits duch.</li>
+        <li>Wir übernehmen den Aufbau und Betrieb ihres Web
+        (frontend) Monitorings.</li>
+        <li>Wir setzen Ihnen eine Bild-CDN Konfiguration auf und
+        kümmern uns um das dazugehörende Zusammenspiel mit
+        ihrem Asset-Management.</li>
+      </ul>
+`,
+    title: 'Web Performance (Monitoring)',
+  },
+  en: {
+    description: 'Impressum description',
+    docTitle: 'Web Performance (Monitoring)',
+    lead: `
+      We see web performance as key enabler towards a sustainable IT.
+      Web Performance is also often times the key factor when it comes to
+      SEO, good Core Web Vitals scores and an outstanding customer experience
+      for your end users.
+    `,
+    richtext: `
+      <p>We can help you analyse and fix performance bottlenecks on existing
+      platforms. In fresh projects we can provide and/or guide architecture
+      decisions and setup a comprehensive Frontend Performance monitoring.
+      We can help you find and define your custom performance metrics.
+      Our strongest frontend performance monitoring experience comes from
+      one of the industry’s most respected tool “SpeedCurve”. We are confident
+      to adopt, should you already have a different tool in place. We also
+      bring a strong experience in working with Image Content Delivery
+      Networks, which are an essential nowadays to ensure an excellent
+      customer experience and a minimal ecological footprint.
+      </p>
+       <h2>What we can do for you</h2>
+       <ul>
+        <li>We accompany architectural decisions to avoid the emergence of
+         performance problems in the first place.</li>
+         <li>We detect and analyze web performance issues on existing
+         platforms/sites.</li>
+        <li>We provide Coding work according to web performance best practices.</li>
+        <li>We can run a performance audit for you.</li>
+        <li>We can setup and operate a web (frontend) performance monitoring.</li>
+        <li>We can setup an image CDN configuration and the corresponding
+        asset handling.</li>
+       </ul>
+    `,
+    title: 'Web Performance (Monitoring)',
+  },
+};
