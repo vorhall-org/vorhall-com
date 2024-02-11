0584de8c3a91b6821fdfe55e419652894208e44e
Marius Bleuer <m.bleuer@lycos.com>
2024-02-11 18:24:53 +0100

    is adding german data privacy text

--- src/i18n/i18n-data-privacy.js
+++ src/i18n/i18n-data-privacy.js
@@ -2,26 +2,284 @@ export default {
   de: {
     description: 'Datenschutzerklärung',
     docTitle: 'Datenschutzerklärung',
-    lead: 'Datenschutzerklärung lead text',
+    lead: 'Version vom 11.02.2024',
     richtext: `
       <p>
-        <strong>Vorhall KLG</strong><br>
-        Brückfeldstrasse 40<br>CH-3012 Bern<br>Schweiz
+        In dieser Datenschutzerklärung erläutern wir, die Vorhall KLG, wie wir Personendaten
+        erheben und sonst bearbeiten. Das ist keine abschliessende Beschreibung; allenfalls
+        regeln andere Datenschutzerklärungen [oder allgemeine Geschäftsbedingungen,
+        Teilnahmebedingungen und ähnliche Dokumente] spezifische Sachverhalte. Unter
+        Personendaten werden alle Angaben verstanden, die sich auf eine bestimmte oder bestimmbare
+        Person beziehen.
       </p>
       <p>
-        <strong>Kontaktmöglichkeit</strong><br>
+        Wenn Sie uns Personendaten anderer Personen (z.B. Familienmitglieder, Daten von Arbeitskol-
+        legen) zur Verfügung stellen, stellen Sie bitte sicher, dass diese Personen die vorliegende
+        Datenschutzerklärung kennen und teilen Sie uns deren Personendaten nur mit, wenn Sie dies dürfen
+        und wenn diese Personendaten korrekt sind.
+      </p>
+      <p>
+        Diese Datenschutzerklärung ist auf die Anforderungen der EU-Datenschutz-Grundverordnung
+        («DSGVO»), das Schweizer Datenschutzgesetz («DSG») und das revidierte Schweizer
+        Datenschutzgesetz («revDSG») ausgelegt. Ob und inwieweit diese Gesetze anwendbar sind, hängt
+        jedoch vom Einzelfall ab.
+      </p>
+      <h2>
+        1. Verantwortlicher / Datenschutz&shy;beauftrager / Vertreter
+      </h2>
+      <p>
+        Verantwortlich für die Datenbearbeitungen, die wir hier beschreiben, ist die Vorhall KLG.
+        Wenn Sie datenschutzrechtliche Anliegen haben, können Sie uns diese an folgende
+        Kontaktadresse mitteilen:
+      </p>
+      <p>
+        Vorhall KLG<br>
+        Brückfeldstrasse 40<br>
+        CH-3012 Bern<br>
         <a href="mailto:info@vorhall.com">info@vorhall.com</a>
       </p>
+      <h2>
+        2. Erhebung und Bearbeitung von Personendaten
+      </h2>
       <p>
-        <strong>UID</strong><br>
-        CHE-371.533.033
+        Wir bearbeiten in erster Linie die Personendaten, die wir im Rahmen unserer
+        Geschäftsbeziehung mit unseren Kunden und anderen Geschäftspartnern von diesen
+        und weiteren daran beteiligten Personen erhalten oder die wir beim Betrieb
+        unserer Websites, Apps und weiteren Anwendungen von deren Nutzern erheben.
       </p>
       <p>
-        <strong>Design</strong><br>
-        Inspiriert und adaptiert von <a href="https://www.figma.com/community/file/1179442320711977498/flowbite-design-system" target="_blank" aria-label="Flowbite Design System. Linkziel öffnet sich in einem neuen Fenster.">Flowbite Design System</a>, veröffentlicht unter <a href="https://creativecommons.org/licenses/by/4.0/deed.de" target="_blank" aria-label="Creative Commons BY 4.0 Lizenz. Linkziel öffnet sich in einem neuen Fenster.">CC BY 4.0 Lizenz</a>.
+        Soweit dies erlaubt ist, entnehmen wir auch öffentlich zugänglichen Quellen (z.B.
+        Betreibungsregister, Grundbücher, Handelsregister, Presse, Internet) gewisse Daten
+        oder erhalten solche von Behörden und sonstigen Dritten. Nebst den Daten von Ihnen,
+        die Sie uns direkt geben, umfassen die Kategorien von Personendaten, die wir von Dritten
+        über Sie erhalten, insbesondere Angaben aus öffentlichen Registern, Angaben, die wir
+        im Zusammenhang mit behördlichen und gerichtlichen Verfahren erfahren, Angaben im
+        Zusammenhang mit ihren beruflichen Funktionen und Aktivitäten (damit wir z.B. mit Ihrer
+        Hilfe Geschäfte mit Ihrem Arbeitgeber abschliessen und abwickeln können), Angaben über Sie
+        in Korrespondenz und Besprechungen mit Dritten, Bonitätsauskünfte (soweit wir mit Ihnen
+        persönlich Geschäfte abwickeln), Angaben über Sie, die uns Personen aus Ihrem Umfeld
+        (Familie, Berater, Rechtsvertreter, etc.) geben, damit wir Verträge mit Ihnen oder unter
+        Einbezug von Ihnen abschliessen oder abwickeln können (z.B. Referenzen, Ihre Adresse für
+        Lieferungen, Vollmachten, Angaben zur Einhaltung gesetzlicher Vorgaben wie etwa der
+        Geldwäschereibekämpfung und Exportrestriktionen, Angaben von Banken, Versicherungen,
+        Vertriebs- und anderen Vertragspartnern von uns zur Inanspruchnahme oder Erbringung
+        von Leistungen durch Sie (z.B. erfolgte Zahlungen, erfolgte Käufe)), Angaben aus Medien und
+        Internet zu Ihrer Person (soweit dies im konkreten Fall angezeigt ist, z.B. im Rahmen einer
+        Bewerbung, Presseschau, Marketing/Verkauf, etc.), Ihre Adressen und ggf. Interessen und weitere
+        soziodemographische Daten (für Marketing), Daten im Zusammenhang mit der Benutzung der
+        Website (z.B. IP-Adresse, MAC-Adresse des Smartphones oder Computers, Angaben zu Ihrem
+        Gerät und Einstellungen, Cookies, Datum und Zeit des Besuchs, abgerufene Seiten und Inhalte,
+        benutzte Funktionen, verweisende Website, Standortangaben).
       </p>
-`,
-    title: 'Datenschutzerklärung',
+      <h2>
+        3. Zwecke der Datenbearbeitung und Rechtsgrundlagen
+      </h2>
+      <p>
+        Wir verwenden die von uns erhobenen Personendaten in erster Linie, um unsere Verträge mit
+        unseren Kunden und Geschäftspartnern abzuschliessen und abzuwickeln, so insbesondere im
+        Rahmen der Beratungs- und Umsetzungsarbeiten für unsere Kunden und den Einkauf von Produkten und
+        Dienstleistungen von unseren Lieferanten, sowie um unseren gesetzlichen Pflichten im In- und
+        Ausland nachzukommen. Wenn Sie für einen solchen Kunden oder Geschäftspartner tätig sind,
+        können Sie in dieser Funktion mit Ihren Personendaten natürlich ebenfalls davon betroffen sein.
+      </p>
+      <p>
+        Darüber hinaus bearbeiten wir Personendaten von Ihnen und weiteren Personen, soweit erlaubt
+        und es uns als angezeigt erscheint, auch für folgende Zwecke, an denen wir (und zuweilen auch
+        Dritte) ein dem Zweck entsprechendes berechtigtes Interesse haben:
+      </p>
+      <ul>
+          <li>
+            Angebot und Weiterentwicklung unserer Angebote, Dienstleistungen und Websites, Apps
+            und weiteren Plattformen, auf welchen wir präsent sind;
+          </li>
+          <li>
+            Kommunikation mit Dritten und Bearbeitung derer Anfragen (z.B. Bewerbungen, Medienanfragen);
+          </li>
+          <li>
+            Prüfung und Optimierung von Verfahren zur Bedarfsanalyse zwecks direkter Kundenansprache
+            sowie Erhebung von Personendaten aus öffentlich zugänglichen Quellen zwecks Kundenakquisition;
+          </li>
+          <li>
+            Werbung und Marketing (einschliesslich Durchführung von Anlässen), soweit Sie der Nutzung
+            Ihrer Daten nicht widersprochen haben (wenn wir Ihnen als bestehender Kunde von
+            uns Werbung zukommen lassen, können Sie dem jederzeit widersprechen, wir setzen Sie
+            dann auf eine Sperrliste gegen weitere Werbesendungen);
+          </li>
+          <li>
+            Markt- und Meinungsforschung, Medienbeobachtung;
+          </li>
+          <li>
+            Geltendmachung rechtlicher Ansprüche und Verteidigung in Zusammenhang mit rechtlichen
+            Streitigkeiten und behördlichen Verfahren;
+          </li>
+          <li>
+            Verhinderung und Aufklärung von Straftaten und sonstigem Fehlverhalten (z.B.
+            Durchführung interner Untersuchungen, Datenanalysen zur Betrugsbekämpfung);
+          </li>
+          <li>
+            Gewährleistungen unseres Betriebs, insbesondere der IT, unserer Website und
+            weiteren Plattformen;
+          </li>
+          <li>
+            Kauf und Verkauf von Geschäftsbereichen, Gesellschaften oder Teilen von Gesellschaften
+            und andere gesellschaftsrechtliche Transaktionen und damit verbunden die Übertragung
+            von Personendaten sowie Massnahmen zur Geschäftssteuerung und soweit zur Einhaltung
+            gesetzlicher und regulatorischer Verpflichtungen sowie interner Vorschriften der
+            Vorhall KLG.
+          </li>
+      </ul>
+      <p>
+        Soweit Sie uns eine Einwilligung zur Bearbeitung Ihrer Personaldaten für bestimmte Zwecke erteilt
+        haben (zum Beispiel bei Ihrer Anmeldung zum Erhalt von Newslettern), bearbeiten wir Ihre
+        Personendaten im Rahmen und gestützt auf diese Einwilligung, soweit wir keine andere
+        Rechtsgrundlage haben und wir eine solche benötigen. Eine erteilte Einwilligung kann
+        jederzeit widerrufen werden, was jedoch keine Auswirkung auf bereits erfolgte Datenbearbeitungen hat.
+      </p>
+      <h2>
+        4. Cookies / Tracking und andere Technologien im Zusammenhang mit der Nutzung unserer Website
+      </h2>
+      <p>
+        Unsere Website <a href="https://vorhall.com">https://vorhall.com</a> verwendet derzeit keine
+        Cookies. Auf der Website läuft der Analytics-Dienst von <a aria-label="Squeaky AI,
+        Linkziel in englischer Sprache, öffnet sich in einem neuen Fenster" href="https://squeaky.ai/"
+        target="_blank" rel="external noopener nofollow">Squeaky AI.</a> Dies ist
+        eine Dienstleistung von Dritten mit Sitz in den Niederlanden, mit welcher wir die Nutzung der
+        Website (nicht personenbezogen) messen und auswerten können. Squeaky nutzt ebenfalls keine Cookies
+        und erfüllt die europäische General Data Protection Regulation (GDPR). Die Analytics-Daten werden
+        in Irland gespeichert und das Tracking erfolgt dabei in anonymisierter Form. Mehr Informationen
+        zur <a aria-label="Squeaky AI Privacy Police, Linkziel in englischer Sprache, öffnet sich in einem neuen Fenster"
+        href="https://squeaky.ai/legal/privacy-policy/" target="_blank" rel="external noopener nofollow">Squeaky AI Privacy
+        Police</a> und <a aria-label="Squaky AI GDPR Compliance, Linkziel in englischer Sprache, öffnet sich
+        in einem neuen Fenster" href="https://squeaky.ai/legal/gdpr/" target="_blank" rel="external
+        noopener nofollow">Squaky AI GDPR-Compliance</a>.
+      </p>
+      <h2>
+        5. Datenweitergabe und Datenübermittlung ins Ausland
+      </h2>
+      <p>
+        Wir geben im Rahmen unserer geschäftlichen Aktivitäten und der Zwecke gemäss Ziff. 3, soweit
+        erlaubt und es uns als angezeigt erscheint, auch Dritten bekannt, sei es, weil sie diese für uns
+        bearbeiten, sei es, weil sie sie für ihre eigenen Zwecke verwenden wollen. Dabei geht es insbe-
+        sondere um folgende Stellen:
+      </p>
+      <ul>
+        <li>
+          Dienstleister von uns (wie z.B. Banken, Versicherungen), einschliesslich Auftragsbearbeitern
+          (wie z.B. IT-Provider);
+        </li>
+        <li>
+          Händler, Lieferanten, Subunternehmer und sonstige Geschäftspartner;
+        </li>
+        <li>
+          Kunden;
+        </li>
+        <li>
+          in- und ausländische Behörden, Amtsstellen oder Gerichten;
+        </li>
+        <li>
+          Medien;
+        </li>
+        <li>
+          Öffentlichkeit, einschliesslich Besuchern von Websites und sozialer Medien;
+        </li>
+        <li>
+          Mitbewerber, Branchenorganisationen, Verbände, Organisationen und weitere Gremien;
+        </li>
+        <li>
+          Erwerber oder Interessenten am Erwerb von Geschäftsbereichen, Gesellschaften oder
+          sonstigen Teilen der XXX-Gruppe;
+        </li>
+        <li>
+          anderen Parteien in möglichen oder tatsächlichen Rechtsverfahren;
+        </li>
+      </ul>
+      <p>
+        alle gemeinsam <strong>Empfänger</strong>.
+      </p>
+      <p>
+        Diese Empfänger sind teilweise im Inland, können aber irgendwo auf der Erde sein. Sie müssen
+        insbesondere mit der Übermittlung Ihrer Daten in alle Länder rechnen, in denen sich die von
+        uns benutzten Dienstleister befinden.
+      </p>
+      <h2>
+        6. Dauer der Aufbewahrung von Personendaten
+      </h2>
+      <p>
+        Wir verarbeiten und speichern Ihre Personendaten, solange es für die Erfüllung unserer
+        vertraglichen und gesetzlichen Pflichten oder sonst die mit der Bearbeitung verfolgten
+        Zwecke erforderlich ist, d.h. also zum Beispiel für die Dauer der gesamten
+        Geschäftsbeziehung (von der Anbahnung, Abwicklung bis zur Beendigung eines Vertrags) sowie
+        darüber hinaus gemäss den gesetzlichen Aufbewahrungs- und Dokumentationspflichten. Dabei
+        ist es möglich, dass Personendaten für die Zeit aufbewahrt werden, in der Ansprüche gegen
+        unser Unternehmen geltend gemacht werden können und soweit wir anderweitig gesetzlich dazu
+        verpflichtet sind oder berechtigte Geschäftsinteressen dies erfordern (z.B. für Beweis- und
+        Dokumentationszwecke). Sobald Ihre Personendaten für die oben genannten Zwecke nicht mehr
+        erforderlich sind, werden sie grundsätzlich und soweit möglich gelöscht oder anonymisiert.
+        Für betriebliche Daten (z.B. Systemprotokolle, Logs), gelten grundsätzliche kürzere
+        Aufbewahrungsfristen von zwölf Monaten oder weniger.
+      </p>
+      <h2>
+        Datensicherheit
+      </h2>
+      <p>
+        Wir treffen angemessene technische und organisatorische Sicherheitsvorkehrungen
+        zum Schutz Ihrer Personendaten vor unberechtigtem Zugriff und Missbrauch durch IT- und
+        Netzwerksicherheitslösungen, Zugangsbeschränkungen, Verschlüsselung von Datenträgern
+        und Übermittlungen und Pseudonymisierung.
+      </p>
+      <h2>
+        8. Pflicht zur Bereitstellung von Personendaten
+      </h2>
+      <p>
+        Im Rahmen unserer Geschäftsbeziehung müssen Sie diejenigen Personendaten bereitstellen, die
+        für die Aufnahme und Durchführung einer Geschäftsbeziehung und der Erfüllung der damit
+        verbundenen vertraglichen Pflichten erforderlich sind (eine gesetzliche Pflicht, uns Daten
+        bereitzustellen, haben Sie in der Regel nicht). Ohne diese Daten werden wir in der Regel nicht in der
+        Lage sein, einen Vertrag mit Ihnen (oder der Stelle oder Person, die Sie vertreten) zu schliessen
+        oder diesen abzuwickeln. Auch die Website kann nicht genutzt werden, wenn gewisse Angaben
+        zur Sicherstellung des Datenverkehrs (wie z.B. IP-Adresse) nicht offengelegt wird.
+      </p>
+      <h2>
+        9. Profiling [und automatisierte Entscheidfindung]
+      </h2>
+      <p>
+        Die Vorhall KLG setzt derzeit kein Profiling ein.
+      </p>
+      <h2>
+        10. Rechte der betroffenen Person
+      </h2>
+      <p>
+        Sie haben im Rahmen des auf Sie anwendbaren Datenschutzrechts und soweit darin vorgesehen
+        (wie etwa im Falle der DSGVO) das Recht auf Auskunft, Berichtigung, Löschung, das Recht auf
+        Einschränkung der Datenbearbeitung und sonst dem Widerspruch gegen unsere Datenbearbeitungen,
+        insbesondere jener für Zwecke des Direktmarketings und weiterer berechtigter Interessen an
+        der Bearbeitung sowie auf Herausgabe gewisser Personendaten zwecks Übertragung an eine andere
+        Stelle (sog. Datenportabilität). Bitte beachten Sie aber, dass wir uns vorbehalten,
+        unsererseits die gesetzlich vorgesehenen Einschränkungen geltend zu machen, etwa wenn wir
+        zur Aufbewahrung oder Bearbeitung gewisser Daten verpflichtet sind, daran ein überwiegendes
+        Interesse haben (soweit wir uns darauf berufen dürfen) oder sie für die Geltendmachung von
+        Ansprüchen benötigen. Falls für Sie Kosten anfallen, werden wir Sie vorab informieren. Über
+        die Möglichkeit, Ihre Einwilligung zu widerrufen, haben wir bereits in Ziff. 3 informiert.
+        Beachten Sie, dass die Ausübung dieser Rechte im Konflikt zu vertraglichen Abmachungen stehen
+        kann und dies Folgen wie z.B. die vorzeitige Vertragsauflösung oder Kostenfolgen haben kann.
+        Wir werden Sie diesfalls vorgängig informieren, wo dies nicht bereits vertraglich geregelt ist.
+      </p>
+      <p>
+        Die Ausübung solcher Rechte setzt in der Regel voraus, dass Sie Ihre Identität eindeutig
+        nachweisen (z.B. durch eine Ausweiskopie, wo Ihre Identität sonst nicht klar ist bzw.
+        verifiziert werden kann). Zur Geltendmachung Ihrer Rechte können Sie uns unter der in
+        Ziffer 1 angegebenen Adresse kontaktieren.
+      </p>
+      <p>
+        Jede betroffene Person hat überdies das Recht, ihre Ansprüche gerichtlich durchzusetzen
+        oder bei der zuständigen Datenschutzbehörde eine Beschwerde einzureichen. Die zuständige
+        Datenschutzbehörde der Schweiz ist der <a aria-label="Eidgenössische Datenschutz- und
+        Öffentlichkeitsbeauftragte, Linkziel öffnet sich in einem neuen Fenster"
+        href="http://www.edoeb.admin.ch" target="_blank" rel="external noopener nofollow">
+        Eidgenössische Datenschutz- und Öffentlichkeitsbeauftragte.</a>
+      </p>
+    `,
+    title: 'Datenschutz&shy;erklärung der Vorhall KLG',
   },
   en: {
     description: 'Data Privacy',
