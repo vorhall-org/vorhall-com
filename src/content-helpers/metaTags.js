cfecf97e72acf57f3470891144caa06961672c91
Yves Tscherry <info@feerglas.com>
2024-02-13 09:24:20 +0100

    move theme color meta's to general meta tags

--- src/content-helpers/metaTags.js
+++ src/content-helpers/metaTags.js
@@ -14,4 +14,14 @@ export const generalMetaTags = (generator) => [
     content: 'bxbg_YknRn8eGXBH7h_ulO_YBwwCGyohGj4Aqkkd1wQ',
     name: 'google-site-verification',
   },
+  {
+    content: '#D6E3FA',
+    media: '(prefers-color-scheme: light)',
+    name: 'theme-color',
+  },
+  {
+    content: '#153376',
+    media: '(prefers-color-scheme: dark)',
+    name: 'theme-color',
+  },
 ];
--- src/content-helpers/metaTagsSeo.js
+++ src/content-helpers/metaTagsSeo.js
@@ -11,16 +11,6 @@ export const seoMetaTags = (translatedData, url, index) => (
         name: 'description',
       },
       {
-        content: '#F9FAFB',
-        media: '(prefers-color-scheme: light)',
-        name: 'theme-color',
-      },
-      {
-        content: '#111827',
-        media: '(prefers-color-scheme: dark)',
-        name: 'theme-color',
-      },
-      {
         content: 'article',
         property: 'og:type',
       },
